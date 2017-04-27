a5eef61522e73d2fe97e2653ea20b0c6be1e7ea9
Michael Mansourati <mmansourati@gmail.com>
2017-04-27 01:21:38 +0000

    good good latest stuff

--- src/Landing/Content.jsx
+++ src/Landing/Content.jsx
@@ -1,7 +1,33 @@
 import React, {Component} from 'react';
-import TopRow from './MainSpace/TopRow.jsx';
-import BottomRow from './MainSpace/BottomRow.jsx';
+// import TopRow from './MainSpace/TopRow.jsx';
+// import BottomRow from './MainSpace/BottomRow.jsx';
 import imgData from './imgdata.js';
+
+// class Content extends Component {
+//   render() {
+//     var top = []
+//     var bottom = []
+//     var counter = 0
+//     for (var data in imgData){
+//       if (counter % 2 == 0 ){
+//         bottom.push(imgData[data])
+//       }else{
+//         top.push(imgData[data])
+//       }
+//       counter ++
+//     }
+//     return(
+
+//       //going to create separate arrays to send to top and bottom in content
+
+//       <div className="ms-cont">
+//         <TopRow arr={top} />
+//         <BottomRow arr={bottom}/>
+//       </div>
+//     );
+
+import TopRow from './Content/TopRow.jsx';
+import BottomRow from './Content/BottomRow.jsx';
 
 class Content extends Component {
   render() {
@@ -25,18 +51,6 @@ class Content extends Component {
         <BottomRow arr={bottom}/>
       </div>
     );
-=======
-import TopRow from './Content/TopRow.jsx';
-import BottomRow from './Content/BottomRow.jsx';
-
-class Content extends Component {
-  render() {
-    return(
-      <div className="ms-cont">
-        <TopRow />
-        <BottomRow />
-      </div>
-    )
   }
 }
 export default Content;
--- src/Landing/Navbar/PlaceSearch.jsx
+++ src/Landing/Navbar/PlaceSearch.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 class PlaceSearch extends Component {
   render() {
-    const disabled = this.props.disableColours
+    const disabled = this.props.disableColors
     const bool = {
       red: disabled.red,
       orange: disabled.orange,
@@ -20,17 +20,17 @@ class PlaceSearch extends Component {
       <div className="ps-cont">
         <form className="ps-form">
           <input type="text" onKeyPress={this.props.enterPlace} className="ps-searchbar" placeholder="e.g. Trinity Bellwoods"></input>
-          <div className="colourbox-cont">
-            <input type="checkbox" disabled={bool.red} onChange={this.props.colourSelect} name="red" className="colourbox cb-red"/>
-            <input type="checkbox" disabled={bool.orange} onChange={this.props.colourSelect} name="orange" className="colourbox cb-orange"/>
-            <input type="checkbox" disabled={bool.yellow} onChange={this.props.colourSelect} name="yellow" className="colourbox cb-yellow"/>
-            <input type="checkbox" disabled={bool.green} onChange={this.props.colourSelect} name="green" className="colourbox cb-green"/>
-            <input type="checkbox" disabled={bool.blue} onChange={this.props.colourSelect} name="blue" className="colourbox cb-blue"/>
-            <input type="checkbox" disabled={bool.purple} onChange={this.props.colourSelect} name="purple" className="colourbox cb-purple"/>
-            <input type="checkbox" disabled={bool.brown} onChange={this.props.colourSelect} name="brown" className="colourbox cb-brown"/>
-            <input type="checkbox" disabled={bool.black} onChange={this.props.colourSelect} name="black" className="colourbox cb-black"/>
-            <input type="checkbox" disabled={bool.grey} onChange={this.props.colourSelect} name="grey" className="colourbox cb-grey"/>
-            <input type="checkbox" disabled={bool.white} onChange={this.props.colourSelect} name="white" className="colourbox cb-white"/>
+          <div className="colorbox-cont">
+            <input type="checkbox" disabled={bool.red} onChange={this.props.colorSelect} name="red" className="colorbox cb-red"/>
+            <input type="checkbox" disabled={bool.orange} onChange={this.props.colorSelect} name="orange" className="colorbox cb-orange"/>
+            <input type="checkbox" disabled={bool.yellow} onChange={this.props.colorSelect} name="yellow" className="colorbox cb-yellow"/>
+            <input type="checkbox" disabled={bool.green} onChange={this.props.colorSelect} name="green" className="colorbox cb-green"/>
+            <input type="checkbox" disabled={bool.blue} onChange={this.props.colorSelect} name="blue" className="colorbox cb-blue"/>
+            <input type="checkbox" disabled={bool.purple} onChange={this.props.colorSelect} name="purple" className="colorbox cb-purple"/>
+            <input type="checkbox" disabled={bool.brown} onChange={this.props.colorSelect} name="brown" className="colorbox cb-brown"/>
+            <input type="checkbox" disabled={bool.black} onChange={this.props.colorSelect} name="black" className="colorbox cb-black"/>
+            <input type="checkbox" disabled={bool.grey} onChange={this.props.colorSelect} name="grey" className="colorbox cb-grey"/>
+            <input type="checkbox" disabled={bool.white} onChange={this.props.colorSelect} name="white" className="colorbox cb-white"/>
           </div>
         </form>
       </div>
