5218022c05f13c49b816841b1889b5b96d543c53
surmistry <mistrs8@gmail.com>
2017-04-27 16:14:51 -0400

    merging finished reactifying component

--- src/Landing/Content/BottomRow.jsx
+++ src/Landing/Content/BottomRow.jsx
@@ -7,8 +7,13 @@ class BottomRow extends Component {
     return(
       <div className="br-cont">
         {
-        arr.map((i) => {
-         return ( <Card url={i.url} key={arr.indexOf(i)}/> )
+          arr.map((i) => {
+            var c1 = `rgb(${i.c1.red}, ${i.c1.green}, ${i.c1.blue})`
+            var c2 = `rgb(${i.c2.red}, ${i.c2.green}, ${i.c2.blue})`
+            var c3 = `rgb(${i.c3.red}, ${i.c3.green}, ${i.c3.blue})`
+            var c4 = `rgb(${i.c4.red}, ${i.c4.green}, ${i.c4.blue})`
+            var colArr = [c1, c2, c3, c4]
+            return ( <Card url={i.url} key={arr.indexOf(i)} colors = {colArr} photographer={i.photog}/> )
           })
         }
       </div>
--- src/Landing/Content/Card.jsx
+++ src/Landing/Content/Card.jsx
@@ -3,19 +3,25 @@
 import React, {Component} from 'react';
 
 class Card extends Component {
+
   render() {
+    var c = this.props.colors
+    let person = this.props.photographer
+    console.log(person)
+    if(person == "")
+      person = "Anonymous"
     return(
       <div className="card">
         <img className="the-goods" src={this.props.url}/>
         <div className="palette-container">
-          <div className="color color-1" ></div>
-          <div className="color color-2"></div>
-          <div className="color color-3"></div>
-          <div className="color color-4"></div>
+          <div className="color color-1" style={{backgroundColor: c[0]}}></div>
+          <div className="color" style={{backgroundColor: c[1]}}></div>
+          <div className="color" style={{backgroundColor: c[2]}}></div>
+          <div className="color color-4" style={{backgroundColor: c[3]}}></div>
           <footer><i className="fa fa-heart-o"></i></footer>
         </div>
         <div className="author-space">
-          <span className="author-name">Author Authorially</span>
+          <span className="author-name">{person}</span>
         </div>
       </div>
     )
